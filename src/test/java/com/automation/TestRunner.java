a63f572a2fff64788e60353dd9fec813da29d633
dgngokhan <67300102+dgngokhan@users.noreply.github.com>
2021-01-03 08:49:24 -0700

    chrome test

--- src/test/java/com/automation/TestRunner.java
+++ src/test/java/com/automation/TestRunner.java
@@ -5,13 +5,20 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.net.URL;
 public class TestRunner {
-    @Test
-    public void calculatorTest() throws Exception {
+    AppiumDriver<AndroidElement> driver;
+    @Before
+    public void setup() throws Exception {
         //desiredCapabilities.setCapability("platformName", "Android");
         //to specify test settings and required info about device and app under the test
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
@@ -29,13 +36,27 @@ public class TestRunner {
         //specify IP/DNS address instead of localhost
         //4723 - default port number of appium server. Can be changed
         URL url = new URL("http://localhost:4723/wd/hub");
-        AppiumDriver<AndroidElement> driver = new AndroidDriver<>(url, desiredCapabilities);
+        driver = new AndroidDriver<>(url, desiredCapabilities);
+    }
+    @Test
+    public void calculatorTest() throws Exception {
         AndroidElement btn2 = driver.findElement(MobileBy.id("com.android.calculator2:id/digit_2"));
         AndroidElement plusBtn = driver.findElement(MobileBy.AccessibilityId("plus"));
         AndroidElement equalsBtn = driver.findElement(MobileBy.AccessibilityId("equals"));
+        AndroidElement resultElement = driver.findElementById("com.android.calculator2:id/result");
+        //to handle synchronization issues, same as in Selenium WebDriver
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.elementToBeClickable(btn2));
         btn2.click(); // 2
         plusBtn.click(); // +
         btn2.click(); // 2
         equalsBtn.click(); // =
+        int expected = 4;
+        int actual = Integer.parseInt(resultElement.getText());
+        Assert.assertEquals(expected, actual);
+    }
+    @After
+    public void tearDown(){
+        driver.closeApp();
     }
 }
